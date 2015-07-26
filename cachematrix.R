76aad42362ca575a22b77f367e02a2b4c6be1203
Eric Borrero <borrero.eric@gmail.com>
2015-07-26 13:22:46 -0400

    Added in short comments describing the functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,14 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+
+## Write a short comment describing this function (makeCacheMatrix):
+## makeCacheMatrix is a function that takes in a matrix, sets the inverse
+## to null since its a new matrix, then creates getters and setters for both
+## the matrix we passed in, and the matrix inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-  mi <- NULL
+  mi <- NULL 
   set <- function(y){
     x <<- y
     mi <<- NULL
@@ -15,10 +19,16 @@ makeCacheMatrix <- function(x = matrix()) {
   list(set = set, get = get,
        setInverse = setInverse,
        getInverse = getInverse)
-
 }
 
-## Write a short comment describing this function
+
+## Write a short comment describing this function (cacheSolve):
+## cacheSolve is a function that takes in an object and checks to see if
+## the object has a function of getInverse, for the matrix inverse. If
+## the inverse of the object is NOT null, it retreives the value from
+## that environment and returns, stopping the function. Otherwise, it
+## uses the get function to obtain the matrix, solves the inverse,
+## and sets the inverse of the object to the new inverse.
 
 cacheSolve <- function(x, ...) {
   mi <- x$getInverse()
